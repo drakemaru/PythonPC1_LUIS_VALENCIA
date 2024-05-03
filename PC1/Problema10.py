aa78820632a884f7b150675467654d204ca740a5
drakemaru <168349483+drakemaru@users.noreply.github.com>
2024-05-03 17:34:19 +0000

    problemas 9 y 10

---
+++ PC1/Problema10.py
@@ -0,0 +1,9 @@
+print('***BIENVENIDO AL PROGRAMA DE ELIMINACIÓN DE ELEMENTOS***')
+elementos = ['Perú','Francia','Inglaterra','España','Alemania','Brazil','Portugal','Turquia']
+print(elementos)
+
+elementos.remove(elementos[5])
+elementos.remove(elementos[4])
+elementos.remove(elementos[0])
+
+print(elementos)
---
+++ PC1/Problema9.py
@@ -0,0 +1,4 @@
+print('***BIENVENIDO AL PROGRAMA QUE INVIERTE LISTAS***')
+lista = [7,'busco',9, 'practicas',10, 'pre', 'profesionales']
+lista.reverse()
+print(lista)
